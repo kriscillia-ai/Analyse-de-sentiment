c41c6e233d8798fc0d3571f89d1b4aed8ce4926e
Kriscillia <133813000+kriscillia-ai@users.noreply.github.com>
2024-06-10 15:08:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
+from textblob import TextBlob
 import pandas as pd
 import streamlit as st
-from textblob import TextBlob
-from polyglot.text import Text
 
 st.header('Analyse de sentiment')
-lang = st.selectbox('Sélectionnez la langue', ['en', 'fr'])
-
 with st.expander('Analyse du text'):
     text = st.text_input('Text ici: ')
     if text:
-        if lang == 'fr':
-            text_polyglot = Text(text, hint_language_code='fr')
-            sentiment_score = text_polyglot.polarity
-            st.write('Polarité: ', round(sentiment_score, 2))
-            st.write('Subjectivité: ', 'Non disponible')
-        else:
-            blob = TextBlob(text)
-            st.write('Polarité: ', round(blob.sentiment.polarity, 2))
-            st.write('Subjectivité: ', round(blob.sentiment.subjectivity, 2))
+        blob = TextBlob(text)
+        st.write('Polarité: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivité: ', round(blob.sentiment.subjectivity,2))
 
 with st.expander('Analyze CSV'):
     upl = st.file_uploader('Téléchargement des fichiers')
 
-    def score(x, lang='en'):
-        if lang == 'fr':
-            text_polyglot = Text(x, hint_language_code='fr')
-            return text_polyglot.polarity
-        else:
-            blob1 = TextBlob(x)
-            return blob1.sentiment.polarity
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
 
+#
     def analyze(x):
         if x >= 0.5:
             return 'Positive'
@@ -38,15 +26,17 @@ with st.expander('Analyze CSV'):
         else:
             return 'Neutral'
 
+#
     if upl:
         df = pd.read_excel(upl)
         del df['Unnamed: 0']
-        df['score'] = df['tweets'].apply(lambda x: score(x, lang))
+        df['score'] = df['tweets'].apply(score)
         df['analysis'] = df['score'].apply(analyze)
         st.write(df.head(10))
 
         @st.cache
         def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
 
         csv = convert_df(df)
